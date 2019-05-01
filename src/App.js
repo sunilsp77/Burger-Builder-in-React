7ba71ad921c73e89e90700463f6432983785f6ad
Sunil Prakash <sunil.prakash@rail.bombardier.com>
2019-05-01 13:22:18 +0530

    Created a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
