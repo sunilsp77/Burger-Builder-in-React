f18e0d11d0a5123c6c72354b06d4d7e259f13081
Sunil Prakash <sunil.prakash@rail.bombardier.com>
2019-05-30 14:29:27 +0530

    Implemented ShouldComponentUpdate in Modal for improving performance, so that Modal and OrderSummary doesn't get re-rendered unnecessariliy

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igkey => {
-    return (
-      <li key={igkey}>
-        <span style={{ textTransform: 'capitalize' }}>{igkey}</span>:{' '}
-        {props.ingredients[igkey]}
-      </li>
-    );
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredient:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={props.purchaseCancel} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={props.purchaseContinue} btnType="Success">
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
 
-export default orderSummary;
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igkey => {
+      return (
+        <li key={igkey}>
+          <span style={{ textTransform: 'capitalize' }}>{igkey}</span>:{' '}
+          {this.props.ingredients[igkey]}
+        </li>
+      );
+    });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredient:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={this.props.purchaseCancel} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={this.props.purchaseContinue} btnType="Success">
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
