5d20155a68ac7658b81e086ef8280611b6aadd8c
Sunil Prakash <sunil.thind17@gmail.com>
2019-06-10 11:43:53 +0530

    Order Submission & passing data between pages

--- src/containers/BugerBuilder/BurgerBuilder.js
+++ src/containers/BugerBuilder/BurgerBuilder.js
@@ -93,30 +93,7 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You Continue!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Sunil',
-    //     address: {
-    //       street: 'Sunpharma-Atladra',
-    //       zipCode: '13123',
-    //       country: 'India',
-    //     },
-    //     email: 'test@test.com',
-    //   },
-    //   deliveryMethod: 'fastest',
-    // };
 
-    // axios
-    //   .post('/orders.json', order)
-    //   .then(response => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   });
     const queryParams = [];
     for (let i in this.state.ingredients) {
       queryParams.push(
@@ -125,6 +102,7 @@ class BurgerBuilder extends Component {
           encodeURIComponent(this.state.ingredients[i]),
       );
     }
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,25 +6,26 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      cheese: 1,
-      meat: 1,
-      bacon: 1,
-    },
+    ingredients: null,
+    totalPrice: 0,
   };
 
-  componentDidMount() {
+  componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     // console.log(query);
     const ingredients = {};
+    let price = 0;
     for (let param of query.entries()) {
-      console.log(param);
-      ingredients[param[0]] = +param[1];
+      if (param[0] === 'price') {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
 
     this.setState({
       ingredients: ingredients,
+      totalPrice: price,
     });
   }
   checkoutCancelledHandler = () => {
@@ -43,7 +44,13 @@ class Checkout extends Component {
         />
         <Route
           path={this.props.match.path + '/contact-data'}
-          component={ContactData}
+          render={props => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+              {...props}
+            />
+          )}
         />
       </div>
     );
