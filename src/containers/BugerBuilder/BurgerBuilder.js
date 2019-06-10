04b7773b30c47ab73def434501f49e1619a591eb
Sunil Prakash <sunil.thind17@gmail.com>
2019-06-10 06:57:38 +0530

    Setting Up routing and routes Navigating to the checkout page

--- src/containers/BugerBuilder/BurgerBuilder.js
+++ src/containers/BugerBuilder/BurgerBuilder.js
@@ -93,30 +93,31 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You Continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Sunil',
-        address: {
-          street: 'Sunpharma-Atladra',
-          zipCode: '13123',
-          country: 'India',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
-    };
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Sunil',
+    //     address: {
+    //       street: 'Sunpharma-Atladra',
+    //       zipCode: '13123',
+    //       country: 'India',
+    //     },
+    //     email: 'test@test.com',
+    //   },
+    //   deliveryMethod: 'fastest',
+    // };
 
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // axios
+    //   .post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   };
 
   render() {
