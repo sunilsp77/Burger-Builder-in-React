d518beb6d46742fa72e42035f88b95791434086b
Sunil Prakash <sunil.thind17@gmail.com>
2019-06-12 08:11:29 +0530

    Fixing a validation flaw

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -42,6 +42,8 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true,
+          minLength: 5,
+          maxLength: 5,
         },
         valid: false,
       },
@@ -111,9 +113,16 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
     if (rules.required) {
-      isValid = value.trim() !== '';
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.minLength && isValid;
     }
     return isValid;
   }
@@ -130,6 +139,7 @@ class ContactData extends Component {
       updatedFormElement.value,
       updatedFormElement.validation,
     );
+    console.log(updatedFormElement);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({
       orderForm: updatedOrderForm,
